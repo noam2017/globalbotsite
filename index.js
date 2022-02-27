7e527558c72bfb488371435025f6b2cc23a81bcd
Red <58099979+RedCommand-dev@users.noreply.github.com>
2022-02-27 21:03:19 +0200

    ☄More content
    Navigation bar, typewriter effect and a lot more

---
+++ index.js
@@ -0,0 +1,19 @@
+const burger = document.querySelector(".burger i");
+const nav = document.querySelector(".nav");
+
+function toggleNav() {
+	burger.classList.toggle("fa-bars");
+	burger.classList.toggle("fa-times");
+	nav.classList.toggle("nav-active");
+}
+
+burger.addEventListener("click", function () {
+	toggleNav();
+});
+
+var typed = new Typed(".input", {
+	strings: ["GlobalBot", "Discord Bot"],
+	typeSpeed: 100,
+	backSpeed: 50,
+	loop: true,
+});
